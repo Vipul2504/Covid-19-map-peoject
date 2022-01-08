b3ff4800e0cc22cb4f311899e3684bf90b7ada0d
Vipul <pawanvishwakarma521@gmail.com>
2022-01-08 12:10:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function updateMap(){
+    fetch('/data.json')
+    .then(response=>response.json())
+    .then(rsp => {
+        console.log(rsp.data)
+        rsp.data.forEach(element => {
+            latitude = element.latitude;
+            longitude = element.longitude;
+            cases = element.infected;
+            if (cases>255){
+                color = "rgb(255,0,0)";
+            }
+            else{
+                color = `rgb(${cases},29,0)`;
+            }
+
+            new mapboxgl.Marker({
+                draggable:false,
+                color:color
+            })
+            .setLngLat([longitude, latitude])
+            .addTo(map);
+        });
+    })
+}
+let interval = 20000;
+updateMap();
